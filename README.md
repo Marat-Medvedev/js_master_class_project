9d1e756e6f272747968b4ffc1ed263807ee11e8d
Marat-Medvedev <63113518+Marat-Medvedev@users.noreply.github.com>
2022-03-23 21:32:10 +0400

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# js_master_class_project
+### *//English version*
+An online calculator for calculating the cost of cargo for a logistics company was made as a reinforcement of the knowledge gained at a JavaScript master class.\
+In this master class, the following were considered:
+
+- JavaScript syntax and its basics
+
+- Working with DOM
+
+- Working with basic algorithms
+### *//Russian version*
+Онлайн-калькулятор для расчета стоимости груза для логистической компании, был выполен в качестве закреплений полученных знаний на мастер-классе по JavaScript.\
+На данном мастер-классе были рассмотрены:
+- Синтаксис JavaScript и его основы
+- Работа с DOM
+- Работа с базовыми алгоритмами
+- An online calculator for calculating the cost of cargo for a logistics company was made as a reinforcement of the knowledge gained at a JavaScript master class.
---
+++ script.js
@@ -0,0 +1,40 @@
+let weight = document.querySelector("#weight"),
+    vol = document.querySelector("#vol"),
+    btn = document.querySelector("#btn"),
+    out = document.querySelector("#out"),
+    load = document.querySelector("#load"),
+    del = document.querySelector("#del"),
+    frag = document.querySelector("#frag"),
+    range = document.querySelector("#range"),
+    dist = document.querySelector(".distance").innerHTML = 500,
+    kg = 5.5,
+    kub = 3500,
+    km = 2;
+//range 
+range.onchange = function() {
+	document.querySelector(".distance").innerHTML = range.value;
+}
+//function
+btn.onclick = function() {
+	if (weight.value !="" && vol.value !="") {
+		if (load.checked) {
+			load.value = 1500;
+		}else {
+			load.value = 0;
+		}
+		if (del.checked) {
+			del.value = 3500;
+		}else {
+			del.value = 0;
+		}
+		if (frag.checked) {
+			frag.value = 2000;
+		}else {
+			frag.value = 0;
+		}
+		let sum = (weight.value * kg) + (vol.value * kub) + Number(load.value) + Number(del.value) + Number(frag.value) + (range.value * km);
+		out.innerHTML = sum;
+	}else {
+		alert('Введите вес и объем груза');
+	}
+}
